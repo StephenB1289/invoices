7e97914e085106fcdd3d0dccbb164739a34a6ad2
Stephen <stephen@branchservices.com>
2022-06-30 20:12:34 -0400

    Continued templating.

--- invoice_template.py
+++ invoice_template.py
@@ -1,4 +1,5 @@
 from fpdf import FPDF
+from requests import head
 
 class invoice(FPDF):
 
@@ -6,23 +7,25 @@ class invoice(FPDF):
         super().__init__(orientation = 'landscape', format = 'letter')
 
     def header(self):
-        self.set_font('helvetica', 'BI', 12)
-        self.cell(30, None, 'Institute for Quantitative Health Science and Engineering (IQ)', align = 'L')
-        self.set_font('helvetica', 'B', 15)
+        self.set_font('helvetica', 'B', 14)
+        header_text = 'Institute for Quantitative Health Science and Engineering (IQ)'
+        self.cell(self.get_string_width(header_text), None, header_text, align = 'L')
+        self.set_font('helvetica', 'B', 35)
         self.set_text_color(175, 175, 175)
-        self.cell(0, None, "INVOICE", align = 'R')
+        self.cell(0, 25, "INVOICE", align = 'C')
         self.ln(6)
-        self.set_font('helvetica', 'BI', 10)
+        self.set_font('helvetica', 'BI', 12)
         self.set_text_color(0, 0, 0)
         self.cell(30, None, '3D Printing Core', align = 'L')
         self.ln(6)
         self.set_font('helvetica', '', 8)
-        self.multi_cell(0, None, 'Michigan State University\n775 Woodlot Drive, IQ Building\nEast Lansing, MI 48824', align = 'L')
+        self.multi_cell(0, 4, 'Michigan State University\n775 Woodlot Drive, IQ Building\nEast Lansing, MI 48824', align = 'L')
+        self.ln(10)
 
 
     def footer(self):
         self.set_y(-30)
-        self.set_font("helvetica", 'BI', 10)
+        self.set_font("helvetica", 'B', 10)
         self.cell(0, None, 'Contact Information:', align = 'C')
         self.ln(5)
         self.set_font('helvetica', '', 10)
--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from invoice_template import invoice
 
-pdf = invoice()
-pdf.add_page()
-pdf.output("invoice.pdf")
+def generate_invoice(details, charges, number):
+    customer, lab, account = details
+    materials, time, core = charges
+
+    pdf = invoice()
+    pdf.add_page()
+    pdf.set_font('helvetica', 'B', 10)
+    pdf.cell(30, None, 'Bill to:', align = 'L')
+    pdf.cell(175, None, 'Date:', align = 'R')
+    pdf.output(number+'.pdf')
+
+details = ('Stephen Branch', 'Spence', '123456')
+charges = (10, 15, 20)
+number = ('test')
+
+generate_invoice(details, charges, number)
