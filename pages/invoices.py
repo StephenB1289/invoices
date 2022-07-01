b36e51a55db5fad8cc424ac4ebde4a16cdcbdba5
Stephen <stephen@branchservices.com>
2022-07-01 14:59:59 -0400

    Added streamlit pages.

---
+++ pages/invoices.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from pdf_templates import invoice
+
+st.text('invoices page')
+
+# details = ('Stephen Branch', 'Spence', '123456')
+# charges = [
+#     ['Labor', 12, 1, 12],
+#     ['Consulting', 10, 0, 0],
+#     ['Post-processing', 15, 1, 15],
+#     ['Print time', 9.10, 1, 9.10], 
+#     ['VeroClear', 0.25, 10, 2.5],
+#     ['SUP706B', 0.1, 20, 2]
+# ]
+# number = 'test'
+
+# pdf = invoice()
+# pdf.generate_invoice(details, charges, number)
---
+++ pages/quotes.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from pdf_templates import quote
+
+st.text('Quotes Page')
+
+# charges = [
+#     ['Labor', 12, 1, 12],
+#     ['Consulting', 10, 0, 0],
+#     ['Post-processing', 15, 1, 15],
+#     ['Print time', 9.10, 1, 9.10], 
+#     ['VeroClear', 0.25, 10, 2.5],
+#     ['SUP706B', 0.1, 20, 2]
+# ]
+
+# pdf2 = quote()
+# pdf2.generate_quote(('Test cust.', 'PI'), charges)
