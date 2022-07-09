b1b01b4c8670a4f7e98fa5d396331932c7ea9f46
Stephen <stephen@branchservices.com>
2022-07-08 21:32:14 -0400

    Got GSheets integration working...

---
+++ pages/Logs.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from google.oauth2 import service_account
+import pygsheets
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+
+client = pygsheets.authorize(custom_credentials = credentials)
+
+sheet = client.open_by_url(st.secrets['sheet_url'])
+
+data = sheet.sheet1.get_as_df()
+
